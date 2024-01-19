bc28e1e5422db131c5d64b69d6161e0be47add0c
空白无上 <hsy040506@163.com>
2024-01-19 10:55:07 +0800

    使用 Kryo 进行序列化

---
+++ rpc-framework-simple/src/main/java/xyz/kbws/serialize/kryo/KryoSerializer.java
@@ -0,0 +1,58 @@
+package xyz.kbws.serialize.kryo;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import xyz.kbws.exception.SerializeException;
+import xyz.kbws.remoting.dto.RpcRequest;
+import xyz.kbws.remoting.dto.RpcResponse;
+import xyz.kbws.serialize.Serializer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @author kbws
+ * @date 2024/1/19
+ * @description: Kryo 序列化类，Kryo 是高性能的序列化/反序列化工具
+ */
+public class KryoSerializer implements Serializer {
+
+    /**
+     * 因为 Kryo 是线程不安全的，所以使用 ThreadLocal 存储 Kryo 对象
+     */
+    private final ThreadLocal<Kryo> kryoThreadLocal = ThreadLocal.withInitial(() -> {
+        Kryo kryo = new Kryo();
+        kryo.register(RpcResponse.class);
+        kryo.register(RpcRequest.class);
+        return kryo;
+    });
+
+    @Override
+    public byte[] serialize(Object obj) {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             Output output = new Output(byteArrayOutputStream)) {
+            Kryo kryo = kryoThreadLocal.get();
+            // Object->byte:将对象序列化为byte数组
+            kryo.writeObject(output, obj);
+            kryoThreadLocal.remove();
+            return output.toBytes();
+        } catch (Exception e) {
+            throw new SerializeException("Serialization failed");
+        }
+    }
+
+    @Override
+    public <T> T deserializer(byte[] bytes, Class<T> clazz) {
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+             Input input = new Input(byteArrayInputStream)) {
+            Kryo kryo = kryoThreadLocal.get();
+            // byte->Object:从byte数组中反序列化出对对象
+            Object o = kryo.readObject(input, clazz);
+            kryoThreadLocal.remove();
+            return clazz.cast(o);
+        } catch (Exception e) {
+            throw new SerializeException("Deserialization failed");
+        }
+    }
+}
