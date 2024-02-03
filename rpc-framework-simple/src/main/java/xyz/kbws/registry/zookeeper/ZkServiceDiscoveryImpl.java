21bb5d18623b98ade7c4b10b7d254bb43ff79fce
空白无上 <hsy040506@163.com>
2024-02-03 19:16:39 +0800

    使用负载均衡进行服务发现

--- rpc-framework-simple/src/main/java/xyz/kbws/registry/zookeeper/ZkServiceDiscoveryImpl.java
+++ rpc-framework-simple/src/main/java/xyz/kbws/registry/zookeeper/ZkServiceDiscoveryImpl.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import xyz.kbws.enums.RpcErrorMessageEnum;
 import xyz.kbws.exception.RpcException;
+import xyz.kbws.loadBalancer.LoadBalance;
+import xyz.kbws.loadBalancer.balancer.RandomLoadBalance;
 import xyz.kbws.registry.ServiceDiscovery;
 import xyz.kbws.remoting.dto.RpcRequest;
 import xyz.kbws.utils.CuratorUtils;
@@ -18,6 +20,13 @@ import java.util.List;
  */
 @Slf4j
 public class ZkServiceDiscoveryImpl implements ServiceDiscovery {
+
+    private final LoadBalance loadBalance;
+
+    public ZkServiceDiscoveryImpl() {
+        this.loadBalance = new RandomLoadBalance();
+    }
+
     @Override
     public InetSocketAddress lookupService(RpcRequest rpcRequest) {
         String rpcServiceName = rpcRequest.getRpcServiceName();
@@ -26,9 +35,9 @@ public class ZkServiceDiscoveryImpl implements ServiceDiscovery {
         if (serviceUrlList.isEmpty()) {
             throw new RpcException(RpcErrorMessageEnum.SERVICE_CAN_NOT_BE_FOUND, rpcServiceName);
         }
-        // load balancing
-        log.info("Successfully found the service address:[{}]", serviceUrlList);
-        String targetServiceUrl = serviceUrlList.get(0);
+        // 负载均衡
+        String targetServiceUrl = loadBalance.selectServiceAddress(serviceUrlList, rpcRequest);
+        log.info("Successfully found the service address:[{}]", targetServiceUrl);
         String[] socketAddressArray = targetServiceUrl.split(":");
         String host = socketAddressArray[0];
         int port = Integer.parseInt(socketAddressArray[1]);
