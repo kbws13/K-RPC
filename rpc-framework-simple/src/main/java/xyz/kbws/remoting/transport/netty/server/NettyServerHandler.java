666059ce860e75fb575247421c7527eddea9e301
空白无上 <hsy040506@163.com>
2024-02-04 21:33:06 +0800

    自定义服务端管道处理

--- rpc-framework-simple/src/main/java/xyz/kbws/remoting/transport/netty/server/NettyServerHandler.java
+++ rpc-framework-simple/src/main/java/xyz/kbws/remoting/transport/netty/server/NettyServerHandler.java
@@ -1,43 +1,90 @@
 package xyz.kbws.remoting.transport.netty.server;
 
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.util.ReferenceCountUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import xyz.kbws.enums.CompressTypeEnum;
+import xyz.kbws.enums.RpcResponseCodeEnum;
+import xyz.kbws.enums.SerializationTypeEnum;
+import xyz.kbws.factory.SingletonFactory;
+import xyz.kbws.remoting.constants.RpcConstants;
+import xyz.kbws.remoting.dto.RpcMessage;
 import xyz.kbws.remoting.dto.RpcRequest;
 import xyz.kbws.remoting.dto.RpcResponse;
-
-import java.util.concurrent.atomic.AtomicInteger;
+import xyz.kbws.remoting.handler.RpcRequestHandler;
 
 /**
  * @author kbws
- * @date 2024/1/20
- * @description: 自定义 ChannelHandler 处理客户端消息
+ * @date 2024/2/3
+ * @description: 自定义服务端的ChannelHandler以处理客户端发送的数据
  */
+@Slf4j
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
 
-    private static final Logger logger = LoggerFactory.getLogger(NettyServerHandler.class);
-    private static final AtomicInteger atomicInteger = new AtomicInteger(1);
+    private final RpcRequestHandler rpcRequestHandler;
+
+    public NettyServerHandler() {
+        this.rpcRequestHandler = SingletonFactory.getInstance(RpcRequestHandler.class);
+    }
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         try {
-            RpcRequest rpcRequest = (RpcRequest) msg;
-            logger.info("server receive msg: [{}] ,times:[{}]", rpcRequest, atomicInteger.getAndIncrement());
-            RpcResponse messageFromServer = RpcResponse.builder().message("message from server").build();
-            ChannelFuture f = ctx.writeAndFlush(messageFromServer);
-            f.addListener(ChannelFutureListener.CLOSE);
+            if (msg instanceof RpcMessage) {
+                log.info("server receive message: [{}]", msg);
+                byte messageType = ((RpcMessage) msg).getMessageType();
+                RpcMessage rpcMessage = new RpcMessage();
+                rpcMessage.setCodec(SerializationTypeEnum.KYRO.getCode());
+                rpcMessage.setCompress(CompressTypeEnum.GZIP.getCode());
+                if (messageType == RpcConstants.HEARTBEAT_REQUEST_TYPE) {
+                    rpcMessage.setMessageType(RpcConstants.HEARTBEAT_RESPONSE_TYPE);
+                    rpcMessage.setData(RpcConstants.PONG);
+                } else {
+                    RpcRequest rpcRequest = (RpcRequest) ((RpcMessage) msg).getData();
+                    // 执行目标方法（客户端需要执行的方法）并返回方法结果
+                    Object result = rpcRequestHandler.handle(rpcRequest);
+                    log.info(String.format("server get result: %s", result.toString()));
+                    rpcMessage.setMessageType(RpcConstants.RESPONSE_TYPE);
+                    if (ctx.channel().isActive() && ctx.channel().isWritable()) {
+                        RpcResponse<Object> rpcResponse = RpcResponse.success(result, rpcRequest.getRequestId());
+                        rpcMessage.setData(rpcResponse);
+                    }else {
+                        RpcResponse<Object> rpcResponse = RpcResponse.fail(RpcResponseCodeEnum.FAIL);
+                        rpcMessage.setData(rpcResponse);
+                        log.error("not writable now, message dropped");
+                    }
+                }
+                ctx.writeAndFlush(rpcMessage).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+            }
+        }catch (Exception e){
+            log.error("occur exception:{}!!!",e.toString());
         } finally {
+            // 确保ByteBuf已释放，否则可能存在内存泄漏
             ReferenceCountUtil.release(msg);
         }
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        logger.error("server catch exception", cause);
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.READER_IDLE) {
+                log.info("idle check happen, so close the connection");
+                ctx.close();
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.error("server catch exception");
+        cause.printStackTrace();
         ctx.close();
     }
 }
