527a218ba37e27c5ca8f437715a76c711bf644e3
空白无上 <hsy040506@163.com>
2024-02-09 21:14:55 +0800

    通道对象存储类

---
+++ rpc-framework-simple/src/main/java/xyz/kbws/remoting/transport/netty/client/ChannelProvider.java
@@ -0,0 +1,48 @@
+package xyz.kbws.remoting.transport.netty.client;
+
+import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author kbws
+ * @date 2024/2/3
+ * @description: 存储并获取通道对象
+ */
+@Slf4j
+public class ChannelProvider {
+    private final Map<String, Channel> channelMap;
+
+    public ChannelProvider() {
+        channelMap = new ConcurrentHashMap<>();
+    }
+
+    public Channel get(InetSocketAddress inetSocketAddress) {
+        String key = inetSocketAddress.toString();
+        // 确定是否存在对应地址的连接
+        if (channelMap.containsKey(key)) {
+            Channel channel = channelMap.get(key);
+            // 如果是，请确定连接是否可用，如果可用，则直接获取
+            if (channel != null && channel.isActive()) {
+                return channel;
+            } else {
+                channelMap.remove(key);
+            }
+        }
+        return null;
+    }
+
+    public void set(InetSocketAddress inetSocketAddress, Channel channel) {
+        String key = inetSocketAddress.toString();
+        channelMap.put(key, channel);
+    }
+
+    public void remove(InetSocketAddress inetSocketAddress) {
+        String key = inetSocketAddress.toString();
+        channelMap.remove(key);
+        log.info("Channel map size :[{}]", channelMap.size());
+    }
+}
