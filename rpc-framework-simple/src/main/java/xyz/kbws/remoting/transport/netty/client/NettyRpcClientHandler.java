c62a1aa4722e7e279ab7d1fd8fe1471e837b59cb
空白无上 <hsy040506@163.com>
2024-02-10 19:46:08 +0800

    Netty管道处理器

---
+++ rpc-framework-simple/src/main/java/xyz/kbws/remoting/transport/netty/client/NettyRpcClientHandler.java
@@ -0,0 +1,87 @@
+package xyz.kbws.remoting.transport.netty.client;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.ReferenceCountUtil;
+import lombok.extern.slf4j.Slf4j;
+import xyz.kbws.enums.CompressTypeEnum;
+import xyz.kbws.enums.SerializationTypeEnum;
+import xyz.kbws.factory.SingletonFactory;
+import xyz.kbws.remoting.constants.RpcConstants;
+import xyz.kbws.remoting.dto.RpcMessage;
+import xyz.kbws.remoting.dto.RpcResponse;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author kbws
+ * @date 2024/2/3
+ * @description: 自定义客户端ChannelHandler以处理服务器发送的数据
+ */
+@Slf4j
+public class NettyRpcClientHandler extends ChannelInboundHandlerAdapter {
+
+    private final UnprocessedRequests unprocessedRequests;
+    private final NettyRpcClient nettyRpcClient;
+
+    public NettyRpcClientHandler() {
+        this.unprocessedRequests = SingletonFactory.getInstance(UnprocessedRequests.class);
+        this.nettyRpcClient = SingletonFactory.getInstance(NettyRpcClient.class);
+    }
+
+    /**
+     * 读取服务端发送的消息
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            log.info("client receive msg: [{}]", msg);
+            if (msg instanceof RpcMessage) {
+                RpcMessage tmp = (RpcMessage) msg;
+                byte messageType = tmp.getMessageType();
+                if (messageType == RpcConstants.HEARTBEAT_RESPONSE_TYPE) {
+                    log.info("heart [{}]", tmp.getData());
+                } else if (messageType == RpcConstants.RESPONSE_TYPE) {
+                    RpcResponse<Object> rpcResponse = (RpcResponse<Object>) tmp.getData();
+                    unprocessedRequests.complete(rpcResponse);
+                }
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.WRITER_IDLE) {
+                log.info("write idle happen [{}]", ctx.channel().remoteAddress());
+                Channel channel = nettyRpcClient.getChannel((InetSocketAddress) ctx.channel().remoteAddress());
+                RpcMessage rpcMessage = new RpcMessage();
+                rpcMessage.setCodec(SerializationTypeEnum.KYRO.getCode());
+                rpcMessage.setCompress(CompressTypeEnum.GZIP.getCode());
+                rpcMessage.setMessageType(RpcConstants.HEARTBEAT_REQUEST_TYPE);
+                rpcMessage.setData(RpcConstants.PING);
+                channel.writeAndFlush(rpcMessage).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+    /**
+     * 在处理客户端消息时发生异常时调用
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.error("client catch exception：", cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
