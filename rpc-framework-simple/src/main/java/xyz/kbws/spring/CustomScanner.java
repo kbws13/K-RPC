a4a0f87e8869fa97393a33d5c8944d2cd4cb1dde
空白无上 <hsy040506@163.com>
2024-02-20 22:19:52 +0800

    自定义包扫描

---
+++ rpc-framework-simple/src/main/java/xyz/kbws/spring/CustomScanner.java
@@ -0,0 +1,25 @@
+package xyz.kbws.spring;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * @author kbws
+ * @date 2024/2/3
+ * @description: 自定义包扫描
+ */
+public class CustomScanner extends ClassPathBeanDefinitionScanner {
+
+    public CustomScanner(BeanDefinitionRegistry registry, Class<? extends Annotation> annoType) {
+        super(registry);
+        super.addIncludeFilter(new AnnotationTypeFilter(annoType));
+    }
+
+    @Override
+    public int scan(String... basePackages) {
+        return super.scan(basePackages);
+    }
+}
