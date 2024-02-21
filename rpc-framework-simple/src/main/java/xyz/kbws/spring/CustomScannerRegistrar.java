2524d1b642f1a5d3553fd73ecee9702bad7a77ae
空白无上 <hsy040506@163.com>
2024-02-21 15:58:57 +0800

    扫描服务注册注解，自动进行服务注册

---
+++ rpc-framework-simple/src/main/java/xyz/kbws/spring/CustomScannerRegistrar.java
@@ -0,0 +1,58 @@
+package xyz.kbws.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.StandardAnnotationMetadata;
+import org.springframework.stereotype.Component;
+import xyz.kbws.annotation.RpcScan;
+import xyz.kbws.annotation.RpcService;
+
+/**
+ * @author kbws
+ * @date 2024/2/3
+ * @description:
+ */
+@Slf4j
+public class CustomScannerRegistrar implements ImportBeanDefinitionRegistrar, ResourceLoaderAware {
+    private static final String SPRING_BEAN_BASE_PACKAGE = "xyz.kbws";
+    private static final String BASE_PACKAGE_ATTRIBUTE_NAME = "basePackage";
+    private ResourceLoader resourceLoader;
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+
+    }
+
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata annotationMetadata, BeanDefinitionRegistry beanDefinitionRegistry) {
+        // 获取RpcScan注释的属性和值
+        AnnotationAttributes rpcScanAnnotationAttributes = AnnotationAttributes.fromMap(annotationMetadata.getAnnotationAttributes(RpcScan.class.getName()));
+        String[] rpcScanBasePackages = new String[0];
+        if (rpcScanAnnotationAttributes != null) {
+            // 获取basePackage属性的值
+            rpcScanBasePackages = rpcScanAnnotationAttributes.getStringArray(BASE_PACKAGE_ATTRIBUTE_NAME);
+        }
+        if (rpcScanBasePackages.length == 0) {
+            rpcScanBasePackages = new String[]{((StandardAnnotationMetadata) annotationMetadata).getIntrospectedClass().getPackage().getName()};
+        }
+        // 扫描RpcService注释
+        CustomScanner rpcServiceScanner = new CustomScanner(beanDefinitionRegistry, RpcService.class);
+        // 扫描组件注释
+        CustomScanner springBeanScanner = new CustomScanner(beanDefinitionRegistry, Component.class);
+        if (resourceLoader != null) {
+            rpcServiceScanner.setResourceLoader(resourceLoader);
+            springBeanScanner.setResourceLoader(resourceLoader);
+        }
+        int springBeanAmount = springBeanScanner.scan(SPRING_BEAN_BASE_PACKAGE);
+        log.info("springBeanScanner扫描的数量 [{}]", springBeanAmount);
+        int rpcServiceCount = rpcServiceScanner.scan(rpcScanBasePackages);
+        log.info("rpcServiceScanner扫描的数量 [{}]", rpcServiceCount);
+
+    }
+}
